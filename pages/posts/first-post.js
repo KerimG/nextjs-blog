0b8c2fe66186dcbca47803a5c8ac8f0f8ae4df82
Kerim Gueney <kerim.gueney@taskforce-it.de>
2020-10-08 13:36:16 +0200

    initial commit (pretty unclean)

---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+// import './first-post.scss';
+
+export default function FirstPost() {
+	return (
+		<Layout>
+			<Head>
+				<title>My First Post</title>
+			</Head>
+			<h1>First Post</h1>
+			<h2>
+				<Link href="/">
+					<a>Back to home</a>
+				</Link>
+			</h2>
+		</Layout>
+	);
+}
