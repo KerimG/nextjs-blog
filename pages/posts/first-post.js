dfaeefbe5b3652cf718697a87a0d5137bb272c7f
Kerim Güney <kerim@gueney.io>
2021-01-31 20:06:07 +0100

    run prettier formatter over all files

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -4,17 +4,17 @@ import Layout from '../../components/layout';
 // import './first-post.scss';
 
 export default function FirstPost() {
-	return (
-		<Layout>
-			<Head>
-				<title>My First Post</title>
-			</Head>
-			<h1>First Post</h1>
-			<h2>
-				<Link href="/">
-					<a>Back to home</a>
-				</Link>
-			</h2>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Head>
+        <title>My First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
 }
